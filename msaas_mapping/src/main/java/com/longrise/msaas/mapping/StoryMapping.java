786de96c39257f936bd96a3c692bcb2ab839703a
ispring <gongjwen@foxmail.com>
2020-07-01 15:22:11 +0800

    idea story 完善章回内容查询逻辑

--- msaas_mapping/src/main/java/com/longrise/msaas/mapping/StoryMapping.java
+++ msaas_mapping/src/main/java/com/longrise/msaas/mapping/StoryMapping.java
@@ -1,11 +1,12 @@
 package com.longrise.msaas.mapping;
 
-
 import com.longrise.msaas.global.domain.EntityBean;
 import com.longrise.msaas.global.excutor.JDBCExcutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
 
 @Repository
 public class StoryMapping {
@@ -15,31 +16,53 @@ public class StoryMapping {
 
     /**
      * 获取故事目录
+     *
      * @return 列表
      */
-    public EntityBean[] getBookList(){
+    public EntityBean[] getBookList() {
         String sql = "select sid, stitlename from storybook";
         return jdbcExcutor.querys(sql);
     }
 
     /**
-     * 获取对应章回内容
-     * @return 列表
+     * 获取当前与之相邻的上下两行的三行章回内容
+     *
+     * @param sid 当前章回的id
+     * @return 当前章回加与之相邻的上下两行的三行数据
      */
-    public EntityBean getBookContent(Long sid){
-        String sql = "select * from storybook where sid=:sid";
+    public EntityBean getBookContent(Long sid) {
+        String sql = "select stitlename, scontent from storybook where sid=:sid";
         EntityBean bean = new EntityBean(1);
         bean.put("sid", sid);
-        return jdbcExcutor.query(sql, bean);
+        bean.put("curr_data", jdbcExcutor.query(sql, bean));
+        sql = "select sign(sid-:sid) as stype, case " +
+                "when sign(sid-:sid)<0 then max(sid) " +
+                "when sign(sid-:sid)>0 then min(sid) " +
+                "else sid " +
+                "end as stitleid " +
+                "from storybook group by stype";
+        EntityBean[] beans = jdbcExcutor.querys(sql, bean);
+        bean.put("prev_sid", "没有了");
+        bean.put("next_sid", "没有了");
+        Arrays.asList(beans).forEach(b -> {
+            String stype = String.valueOf(b.get("stype"));
+            if ("-1".equals(stype)) {
+                bean.put("prev_sid", b.get("stitleid"));
+            } else if ("1".equals(stype)) {
+                bean.put("next_sid", b.get("stitleid"));
+            }
+        });
+        return bean;
     }
 
     /**
      * 批量向数据库新增故事书数据
+     *
      * @param beans Map数组
-     * @return
+     * @return 返回对应新增所影响的行数集合
      */
-    @Transactional(rollbackFor=Exception.class)
-    public boolean insetBookInfo(EntityBean[] beans){
+    @Transactional(rollbackFor = Exception.class)
+    public boolean insetBookInfo(EntityBean[] beans) {
         String sql = "insert into storybook (sid, createtime, sbookname, stitlename, scontent) " +
                 "values (:sid, :createtime, :sbookname, :stitlename, :scontent)";
         return jdbcExcutor.insertUpdateDeletes(sql, beans);
--- msaas_web/src/main/java/com/longrise/msaas/web/controller/StoryController.java
+++ msaas_web/src/main/java/com/longrise/msaas/web/controller/StoryController.java
@@ -1,17 +1,13 @@
 package com.longrise.msaas.web.controller;
 
-
 import com.longrise.msaas.global.domain.EntityBean;
 import com.longrise.msaas.service.StoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/book")
