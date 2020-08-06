db69b71a5f0520e9f96bbe826bead8aea2837615
ispring <gongjwen@foxmail.com>
2020-08-06 15:54:38 +0800

    [idea-win] 全局异常处理增加日志格式化

--- msaas_global/src/main/java/com/longrise/msaas/global/handler/APIExceptionHandler.java
+++ msaas_global/src/main/java/com/longrise/msaas/global/handler/APIExceptionHandler.java
@@ -3,6 +3,8 @@ package com.longrise.msaas.global.handler;
 import com.longrise.msaas.global.domain.APIException;
 import com.longrise.msaas.global.domain.ResultStatus;
 import com.longrise.msaas.global.domain.ResultV0;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,8 +15,12 @@ import javax.servlet.http.HttpServletRequest;
  */
 @RestControllerAdvice
 public class APIExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(APIException.class);
     @ExceptionHandler(Exception.class)
     public ResultV0<String> ExceptionHandler(HttpServletRequest r, Exception e) {
+
+        logger.error(String.format("%s with %s", e.toString(), e.getStackTrace()[0]));
+
         // 注意哦，这里返回类型是自定义响应体
         ResultV0<String> rv0 = new ResultV0<>(ResultStatus.ERROR, r.getRequestURI(), e.getMessage());
         Class<?> clazz = e.getClass();
